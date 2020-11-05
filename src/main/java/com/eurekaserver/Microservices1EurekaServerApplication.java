5aba4d10d0dfbd68eed17ac35abae9cc1b234667
Omer Can Korkmaz <ockorkmaz@ekin.com>
2020-11-05 17:45:09 +0300

    INITIAL

--- src/main/java/com/eurekaserver/Microservices1EurekaServerApplication.java
+++ src/main/java/com/eurekaserver/Microservices1EurekaServerApplication.java
@@ -2,14 +2,22 @@ package com.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class Microservices1EurekaServerApplication {
+public class Microservices1EurekaServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Microservices1EurekaServerApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Microservices1EurekaServerApplication.class);
+    }
+	
 
 }
