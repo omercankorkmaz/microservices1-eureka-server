04b24eeaebe136888787d8f166dd1d9440381e63
Ömer Can Korkmaz <omercankorkmaz0@gmail.com>
2020-11-05 09:33:15 +0300

    Add files via upload
    INITIAL

---
+++ src/main/java/com/eurekaserver/Microservices1EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservices1EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservices1EurekaServerApplication.class, args);
+	}
+
+}
